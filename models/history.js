4a05a9d53b1178adc246543a6f5746ea11ed5f02
Sooraj <soorajnraju@gmail.com>
2020-06-07 22:26:14 +0530

    geolib added and history added into vehicle schema

--- models/history.js
+++ models/history.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_DB_CONN_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
 var Schema = mongoose.Schema;
 
-var vehicleSchema = new Schema({
-    uid: {type: String, unique : true, required: true},
-    name: {type: String, required: true},
+var historySchema = new Schema({
+    vehicle: { type: Schema.Types.ObjectId, ref: 'Vehicle' },
+    uid: {type: String, required: true},
     pos_lat: {type: String, required: true},
     pos_lon: {type: String, required: true},
-    history: []
+    timestamp: {type: Number, required: true},
     },
     {timestamps: true}
 );
 
-module.exports = mongoose.model('Vehicle', vehicleSchema);
+module.exports = mongoose.model('History', historySchema);
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const geolib = require('geolib');
 var Vehicle = require('../models/vehicle');
+var History = require('../models/history');
 
 
 /* GET all vehicle. */
 router.get('/list', function (req, res, next) {
-  Vehicle.find({}, function(err, result) {
+  Vehicle.find({}, function (err, result) {
     if (err) {
       res.json({
         status: "error",
@@ -25,9 +27,9 @@ router.get('/list', function (req, res, next) {
 });
 
 /** GET one*/
-router.get('/:id', function (req, res, next) {
+router.get('/fetch-one/:id', function (req, res, next) {
   let _id = req.params.id;
-  Vehicle.find({_id: _id}, function(err, result) {
+  Vehicle.find({ _id: _id }, function (err, result) {
     if (err) {
       res.json({
         status: "error",
@@ -48,10 +50,17 @@ router.get('/:id', function (req, res, next) {
 
 /* POST create vehicle. */
 router.post('/create', function (req, res, next) {
-  var data = req.body;
+  let data = req.body;
   //console.log(data);
-  const vehicle = new Vehicle({ uid: data.uid, name: data.name, pos_lat: data.pos_lat, pos_lon: data.pos_lon });
-  vehicle.save().then(() => {
+
+  var vehicle = new Vehicle({ uid: data.uid, name: data.name, pos_lat: data.pos_lat, pos_lon: data.pos_lon });
+  
+  vehicle.save().then((vehicle) => {
+    const history_obj = { vehicle: vehicle, uid: data.uid, pos_lat: data.pos_lat, pos_lon: data.pos_lon, timestamp: Date.now() };
+    //vehicle.history.push(history_obj);
+    //vehicle.save();
+    const history = new History(history_obj);
+    history.save();
     res.json({
       status: "success",
       data: {
@@ -71,8 +80,78 @@ router.post('/create', function (req, res, next) {
 });
 
 /* POST update a vehicle. */
-router.post('/update', function (req, res, next) {
-  res.send('update a vehicle');
+router.post('/update', async function (req, res, next) {
+  let data = req.body;
+  let vehicle = await Vehicle.findOne({ _id: data._id });
+
+  vehicle.name = data.name;
+  vehicle.pos_lat = data.pos_lat;
+  vehicle.pos_lon = data.pos_lon;
+
+  const history_obj = { vehicle: vehicle, uid: data.uid, pos_lat: data.pos_lat, pos_lon: data.pos_lon, timestamp: Date.now() };
+  const vehicle_history_obj = { vehicle: vehicle._id, uid: data.uid, pos_lat: data.pos_lat, pos_lon: data.pos_lon, timestamp: Date.now() };
+  
+  const history = new History(history_obj);
+
+  vehicle.history.push(vehicle_history_obj);
+
+  vehicle.save().then(() => {
+    history.save();
+    res.json({
+      status: "success",
+      data: {
+        vehicle: vehicle
+      },
+      message: "Vehicle updated successfuly"
+    });
+  }).catch((error) => {
+    res.json({
+      status: "error",
+      data: {
+        vehicle: vehicle
+      },
+      message: "Failed to update"
+    });
+  });
 });
+
+/* GET all vehicle history. */
+router.get('/history', function (req, res, next) {
+  History.
+    find({}).
+    populate('vehicle').
+    exec(function (err, history) {
+      if (err) return handleError(err);
+      res.json(history);
+    });
+});
+
+/* GET vehicle history by id. */
+router.get('/history/:id', function (req, res, next) {
+  let _id = req.params.id;
+  History.
+    find({ vehicle: _id }, function (err, result) {
+      if (err) return handleError(err);
+      res.json(result);
+    });
+});
+
+/**
+ * 
+ * @param {latitude 1} lat1 
+ * @param {longitude 1} lon1 
+ * @param {timestamp 1} t1 
+ * @param {latitude 2} lat2 
+ * @param {longitude 2} lon2 
+ * @param {timestamp 2} t2 
+ */
+function getVehicleSpeed(lat1, lon1, t1, lat2, lon2, t2) {
+  let speed = geolib.getSpeed(
+    { latitude: lat1, longitude: lon1, time: t1 },
+    { latitude: lat2, longitude: lon2, time: t2 }
+  );
+
+  return speed;
+}
 
 module.exports = router;
